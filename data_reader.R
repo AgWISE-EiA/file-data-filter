534f682e1c880cd42438b479ce1a5de1e66724b7
Sammy Barasa <barsamms@gmail.com>
2023-09-18 10:37:51 +0300

    feat: updated function to take dynamic filter list
    the filter list will be an array contining the column, comparator and value
    
    BREAKING CHANGE:

--- data_reader.R
+++ data_reader.R
@@ -1,55 +1,37 @@
-#' Read Excel File and Optionally Filter Data
-#'
-#' This function reads an Excel file located at the specified path and optionally
-#' filters the data based on a specified column and filter value.
-#'
-#' @param file_path A character string specifying the path to the Excel file.
-#' @param sheet_name A character string specifying the name of the sheet to read
-#'                  from the Excel file (default is NULL, which reads the first sheet).
-#' @param column_name A character string specifying the name of the column to use for filtering
-#'                   (default is NULL, which means no filtering).
-#' @param filter_value A value to filter the data in the specified column by
-#'                    (default is NULL, which means no filtering).
-#'
-#' @return A data frame containing the Excel data. If filtering is applied,
-#'         the resulting data frame will contain only the rows that meet the filtering criteria.
-#' @export
-#'
-#' @examples
-#' # Read an Excel file without filtering
-#' data <- read_excel_file("your_excel_file.xlsx")
-#'
-#' # Read an Excel file and filter the data
-#' filtered_data <- read_excel_file("your_excel_file.xlsx", sheet_name = "Sheet1",
-#'                                  column_name = "ColumnName", filter_value = "FilterValue")
-#'
-#' # View the resulting data
-#' head(filtered_data)
-#'
-#' @seealso \code{\link{readxl::read_excel}}
-#' @seealso \code{\link[dplyr]{filter}}
-#'
-#' @importFrom readxl read_excel
-#' @importFrom dplyr filter
-#'
-read_excel_file <- function(file_path, sheet_name = NULL, column_name = NULL, filter_value = NULL) {
+read_and_filter_excel <- function(file_path,sheet_name = NULL, filters = list()) {
   # Check if the file exists
   if (!file.exists(file_path)) {
     stop("File not found.")
   }
   
-  # Read the Excel file into a data frame
   if (is.null(sheet_name)) {
     data <- readxl::read_excel(file_path)
   } else {
     data <- readxl::read_excel(file_path, sheet = sheet_name)
   }
   
-  if (!is.null(column_name) && !is.null(filter_value)) {
-    print(paste("Filtering data using column [", column_name, "] with value [", filter_value, "]", sep = ''))
-    filtered_df <- data %>% dplyr::filter(get(column_name) == filter_value)
-    return(filtered_df)
+  # Check if filters are specified
+  if (length(filters) > 0) {
+    for (filter in filters) {
+      column_name <- filter$column_name
+      comparator <- filter$comparator
+      filter_value <- filter$filter_value
+      
+      # Apply the filter based on the specified column and comparator
+      if (comparator == "==") {
+        data <- data %>% dplyr::filter(get(column_name) == filter_value)
+      } else if (comparator == "!=") {
+        data <- data %>% dplyr::filter(get(column_name) != filter_value)
+      } else if (comparator == ">") {
+        data <- data %>% dplyr::filter(get(column_name) > filter_value)
+      } else if (comparator == "<") {
+        data <- data %>% dplyr::filter(get(column_name) < filter_value)
+      }
+      
+      # You can add more comparators as needed
+    }
   }
   
+  # Return the resulting data frame
   return(data)
 }
--- demo.R
+++ demo.R
@@ -11,10 +11,15 @@ column_to_filter <- "fertilizer"  # Replace with the actual column name
 filter_value <- "DAP"
 sheet_name=NULL
 
-result <- read_excel_file(
-  file_path = file_path,
-  column_name=column_to_filter,
-  filter_value=filter_value)
+result <- read_excel_file(file_path = file_path,column_name=column_to_filter, filter_value=filter_value)
+
+
+filters <- list(
+  list(column_name = "fertilizer", comparator = "==", filter_value = "DAP"),
+  list(column_name = "yieldClass", comparator = "<", filter_value = 5)
+)
+
+result <- read_and_filter_excel(file_path = file_path,filters = filters)
 
 #result <- read_excel_file(file.choose())
 
